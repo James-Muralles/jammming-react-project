2281541213d49286a7960ade0f83a7a2184dda12
James Muralles <james.muralles23@gmail.com>
2020-06-20 22:53:30 -0500

    working on adding all the components

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Track.css';
+
+class Track extends React.Component {
+    render(){
+        return <div className="Track">
+            <div className="Track-information">
+                <h3>Born Sinner</h3>
+                <p>J.Cole| Born Sinner</p>
+            </div>
+            <button isRemoval={} className="Track-action"><!-- + or - will go here --></button>
+        </div>
+    }
+
+
+}
+export default Track;
