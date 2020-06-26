a5e390efb226a9c1b34ae06c3d4f139f72e42381
James Muralles <james.muralles23@gmail.com>
2020-06-25 19:29:37 -0500

    fixed api search

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
 const clientId = '26df516764264a91b35de82ff1e490fa';
-const redirectUri = 'http://localhost:3000/';
+const redirectUri = 'http://localhost:3000/callback/';
 let accessToken;
 const Spotify = {
     getAccessToken() {
@@ -8,25 +8,23 @@ const Spotify = {
         }
 
         const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
-        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/)
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
         if (accessTokenMatch && expiresInMatch) {
             accessToken = accessTokenMatch[1];
             const expiresIn = Number(expiresInMatch[1]);
             window.setTimeout(() => accessToken = '', expiresIn * 1000);
-            window.history.pushState('Access Token', null, '/');
-            return accessToken
+            window.history.pushState('Access Token', null, '/'); // This clears the parameters, allowing us to grab a new access token when it expires.
+            return accessToken;
         } else {
-            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}_ID&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}
-`;
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
             window.location = accessUrl;
         }
-
     },
 
     search(searchTerm) {
         const accessToken = Spotify.getAccessToken();
-        return fetch(`https://api.spotify.com/v1/search?type=track&q=${searchTerm}`, {
-            header: {
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${searchTerm}`,{
+            headers: {
                 Authorization: `Bearer ${accessToken}`
             }
         }).then(response => {
@@ -34,15 +32,16 @@ const Spotify = {
         }).then(jsonResponse => {
             if (!jsonResponse.tracks) {
                 return [];
-
             }
-            return jsonResponse.tracks.items.map(track => ({
-                id: track.id,
-                name: track.name,
-                artists: track.artists[0].name,
-                album: track.album.name,
-                uri: track.uri
-            }))
+            return jsonResponse.tracks.items.map(track => {
+                return {
+                    id: track.id,
+                    name: track.name,
+                    artists: track.artists[0].name,
+                    album: track.album.name,
+                    uri: track.uri
+                }
+            })
         })
     },
 
